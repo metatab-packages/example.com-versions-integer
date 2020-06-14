ab041343ecf425ec40a982003293770f52441f36
Eric Busboom <eric@civicknowledge.com>
2020-06-14 15:35:34 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# example.com-versions-integer-1
---
+++ pylib/__init__.py
@@ -0,0 +1,38 @@
+""" Example pylib functions"""
+
+
+def row_generator(resource, doc, env, *args, **kwargs):
+    """ An example row generator function.
+
+    Reference this function in a Metatab file as the value of a Datafile:
+
+            Datafile: python:pylib#row_generator
+
+    The function must yield rows, with the first being headers, and subsequenct rows being data.
+
+    :param resource: The Datafile term being processed
+    :param doc: The Metatab document that contains the term being processed
+    :param args: Positional arguments passed to the generator
+    :param kwargs: Keyword arguments passed to the generator
+    :return:
+
+
+    The env argument is a dict with these environmental keys:
+
+    * CACHE_DIR
+    * RESOURCE_NAME
+    * RESOLVED_URL
+    * WORKING_DIR
+    * METATAB_DOC
+    * METATAB_WORKING_DIR
+    * METATAB_PACKAGE
+
+    It also contains key/value pairs for all of the properties of the resource.
+
+    """
+
+    yield 'a b c'.split()
+
+    for i in range(10):
+        yield [i, i*2, i*3]
+
